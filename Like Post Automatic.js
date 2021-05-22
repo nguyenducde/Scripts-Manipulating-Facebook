6a40c19b2df898d6bdfba9f6b83af7ff8f5ab21a
nguyenducde <ducde678@gmail.com>
2021-05-22 14:06:33 +0700

    first commit

---
+++ Like Post Automatic.js
@@ -0,0 +1,26 @@
+//Like Post Automatic
+(() => {
+    setTimeout(function continueWhenPageLoad() {
+        var articles = document.getElementsByClassName('_1ekf');
+        //console.log("Sô lượng bài viết: " + articles.length)
+        var statusLike = document.getElementsByClassName('img _mpx img _4s0y');
+        let articleIndex = 0;
+        setTimeout(function clickNextButton() {
+            articles[articleIndex].click();
+              for (j = 0; j < articles.length; j++){
+                    if (j == 0) {
+                        statusLike[j].click();
+                    }
+                }
+               // console.log("Vị trí bài viết:"+articleIndex+" Vị trí lặp: "+articles.length)
+            if (articleIndex>articles.length) {
+                window.scrollTo(0, document.body.scrollHeight);
+                setTimeout(continueWhenPageLoad, 3000);
+                return;
+            }
+            articleIndex++;
+            setTimeout(clickNextButton, 1000);
+        });
+          
+    }, 0);
+})();
