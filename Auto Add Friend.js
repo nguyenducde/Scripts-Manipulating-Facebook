1546ed114cc5abbdfcbb0821dc6d9eaf724e4095
nguyenducde <ducde678@gmail.com>
2021-05-22 20:01:17 +0700

    Auto Add Friend New

--- Auto Add Friend.js
+++ Auto Add Friend.js
@@ -1,8 +1,9 @@
-/*Chỉnh thành https://m.facebook.com */
+/*Chỉnh thành https://m.facebook.com/friends/center/suggestions/?_rdr */
 /** Mỗi 4 giây sẽ tự động add bạn bè */
 (() => {
      setTimeout(function continueWhenPageLoad() {
          listFriend = document.getElementsByClassName("_55sr");
+         console.log("Danh sách kết bạn " + listFriend.length);
          let buttonIndex = 0;
         setTimeout(function clickNextButton() {
             if (listFriend[buttonIndex].innerHTML == "Thêm bạn bè")
@@ -12,14 +13,15 @@
             if (buttonIndex > listFriend.length)
             {
                  window.scrollTo(0, document.body.scrollHeight);
-                setTimeout(continueWhenPageLoad, 2000);
+                setTimeout(continueWhenPageLoad, 1000);
                 return;
                 }
             buttonIndex++;
-            setTimeout(clickNextButton, 4000);
+            setTimeout(clickNextButton, 2000);
            
         }); 
     }, 0);
 })();
 /**Code by Nguyễn Đức Đề */
 /**Link facebook: https://www.facebook.com/nguyenducde.meo */
+
