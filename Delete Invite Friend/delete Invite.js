a868132c6ae77e5fdda3efd65a3b1e2cd9ef849f
nguyenducde <ducde678@gmail.com>
2021-05-25 16:18:18 +0700

    <3

--- Delete Invite Friend/delete Invite.js
+++ Delete Invite Friend/delete Invite.js
@@ -1,27 +1,25 @@
-/*Chỉnh thành https://m.facebook.com/send_page_invite/?pageid=101480562131909&reference=msite_friends_inviter_card */
-/** Mời hàng loạt bạn bè like fanpage */
 (() => {
      setTimeout(function continueWhenPageLoad() {
          listFriend = document.getElementsByClassName("_55sr");
          let buttonIndex = 0;
          let count = 0;
         setTimeout(function clickNextButton() {
-            if (listFriend[buttonIndex].innerHTML == "Mời")
+            if (listFriend[buttonIndex].innerHTML == "Hủy")
             {
                 listFriend[buttonIndex].click();
+                console.log("Hủy lời mời thứ " + count + " Trong tổng " + listFriend.length);
                 count++;
-                console.log("Đã mời " + count + " bạn like page trong số " + listFriend.length);
             }
            
             if (buttonIndex > listFriend.length)
             {
                 
-                setTimeout(continueWhenPageLoad, 7000);
+                setTimeout(continueWhenPageLoad, 500);
                 return;
             }
              window.scrollTo(0, document.body.scrollHeight);
             buttonIndex++;
-            setTimeout(clickNextButton, 10000);
+            setTimeout(clickNextButton, 1000);
            
         }); 
     }, 0);
